3b5e705ca92ab2bfb5901dde7164f3be13d065a6
shraddhamunavalli <munavallishraddha4@gmail.com>
2025-11-24 16:36:35 +0530

    Update add function to handle command line inputs
    Modified the main execution block to accept command line arguments for addition, with a fallback to default values.

--- addition.py
+++ addition.py
@@ -1,6 +1,14 @@
-def add(a,b):
+import sys
+
+def add(a, b):
     return a + b
-if __name__ == "__main__":
-    x = 10
-    y = 20
-    print("sum :", add(x,y))
+
+if name == "main":
+    if len(sys.argv) == 3:  
+        x = int(sys.argv[1])
+        y = int(sys.argv[2])
+    else:  
+        x = 10
+        y = 20
+
+    print("sum", add(x, y))
